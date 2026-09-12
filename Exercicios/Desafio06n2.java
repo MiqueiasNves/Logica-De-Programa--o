cef639d8c3a34a69e8e5e7ab8979a2dc4a406d88
Miqueias Pereira Neves <Miqueiasneves17@gmail.com>
2026-09-12 01:59:24 -0300

    Create Desafio06n2.java

---
+++ Exercicios/Desafio06n2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Desafio06n2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = sc.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = sc.nextDouble();
+
+        double media = (nota1 + nota2) / 2;
+
+        if (media >= 7) {
+            System.out.println("Média: " + media);
+        } else {
+            System.out.println("Média: " + media);
+        }
+
+        System.out.println("Situação: " + (media >= 7 ? "Aprovado" : "Reprovado"));
+
+        sc.close();
+    }
+}
